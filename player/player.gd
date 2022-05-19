49c3f2a7fa8c3ad039356a0b9cf6b932b889c386
monica <impossibleless@gmail.com>
2022-05-19 12:16:02 +0800

    implement player some action

---
+++ player/player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+export var speed = 480
+var screen_size
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0 :
+		velocity = velocity.normalized()*speed
+
