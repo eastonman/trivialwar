fa1b20948cfec1cf1fb3b41276983ed747a4cef1
Easton Man <manyang.me@outlook.com>
2022-06-02 11:07:10 +0800

    feat: add leaderboard ctrl

--- rankingPage/leader_board.gd
+++ rankingPage/leader_board.gd
@@ -2,6 +2,11 @@ extends ScrollContainer
 
 var entry_scene = preload("res://rankingPage/LeaderBoardEntry.tscn")
 func display(json_raw):
+	# Remove all old entries
+	for child in get_children():
+		remove_child(child)
+		
+	# Add new entries
 	var json_res = JSON.parse(json_raw).result
 	print(json_res)
 	for item in json_res:
